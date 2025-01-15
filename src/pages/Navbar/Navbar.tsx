e8df6dc0e077121a6908c77d6fb1556db1c3da09
Carrington <hartis.c99@gmail.com>
2025-01-14 22:00:21 -0500

    started search functionality

--- src/pages/Navbar/Navbar.tsx
+++ src/pages/Navbar/Navbar.tsx
@@ -1,10 +1,21 @@
 import "./navbar.css";
 import MultiLevelDropdown from "../MultiLevelDropdown/MultiLevelDropdown";
-import { MouseEvent, useContext } from "react";
+import { MouseEvent, KeyboardEvent, useContext, useState } from "react";
 import { MealContext } from "../../Context/mealContext";
 
 export const Navbar = () => {
   const { selectedMeal, setSelectedMeal } = useContext(MealContext);
+  const [searchItem, setSearchItem] = useState("");
+  const [searchFilteredMeals, setSearchFilteredMeals] = useState([]);
+
+  const handleInputChange = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.target instanceof HTMLInputElement) {
+      const searchTerm = event.target.value;
+      setSearchItem(searchTerm);
+    }
+  };
+  
+  console.log(searchItem);
 
   const handleMealClick = (event: MouseEvent<HTMLAnchorElement>) => {
     if (event.target instanceof HTMLAnchorElement) {
@@ -28,16 +39,27 @@ export const Navbar = () => {
     <>
       <div className="navbar">
         <div className="header">
-          <p>cooking is a <span className="underline">science</span>, tasting is an <span className="underline">art</span></p>
+          <p>
+            cooking is a <span className="underline">science</span>, tasting is an{" "}
+            <span className="underline">art</span>
+          </p>
           <h1 className="app-header">Kitchen Alchemy</h1>
-          <input className="search-bar"></input>
+          <input
+            className="search-bar"
+            type="text"
+            value={searchItem}
+            onChange={handleInputChange}
+            placeholder="Type to search"
+          ></input>
         </div>
         <div className="filter-contents">
           {/* Browse categories */}
           {MultiLevelDropdown()}
           {/* Browse meals */}
           <div className="meal-tags">
-            <p className="emphasis" id="browse-meal">browse by meal :</p>
+            <p className="emphasis" id="browse-meal">
+              browse by meal :
+            </p>
             {["breakfast", "salad", "soup", "lunch", "dinner"].map((meal) => (
               <a
                 key={meal}
